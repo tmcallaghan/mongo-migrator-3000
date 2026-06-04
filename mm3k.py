adf511be6becd6e16a53d4af56502fc98646eae2
Tim Callaghan <tmcallaghan@gmail.com>
2026-06-04 15:21:58 -0400

    Update mm3k.py

--- mm3k.py
+++ mm3k.py
@@ -154,8 +154,9 @@ def main():
 
     mp.set_start_method('spawn')
     q = mp.Manager().Queue()
+    #tController = threading.Thread(target=segmenter,args=(appConfig,q))
 
-    tController = threading.Thread(target=segmenter,args=(appConfig,q))
+    tController = threading.Thread(target=segmenter,args=(appConfig))
     tController.start()
 
     #t = threading.Thread(target=reporter,args=(appConfig,q))
